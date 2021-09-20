ff71da72c93c4b9c6bd716fa1d21fe5ddbaa8374
mutyasowmya <mutyasowmya@gmail.com>
2021-09-20 13:43:20 +0530

    added test method

---
+++ sampleapp/src/main/java/sampleapp/testClass.java
@@ -0,0 +1,10 @@
+package sampleapp;
+
+public class testClass {
+public void test() {
+	System.out.println("Test class validated");
+	System.out.println("New features added........");
+	System.out.println("Test class validated");
+	System.out.println("Test class validated");
+}
+}
